ab6b70b8d8ec5693d3c48a1b3665229339279c43
josealves94 <jose.alves94@gmail.com>
2019-05-22 20:11:10 +0200

    Update delete_volumes_ebs.sh
    update aws cli

--- delete_volumes_ebs.sh
+++ delete_volumes_ebs.sh
@@ -7,10 +7,12 @@
 #
 #####################
 
-echo "Nombre d'instance à supprimer : "
+echo "Nombre de volumes disponibles à supprimer : "
 aws ec2 describe-volumes  | grep available | awk '{print $9}' |wc -l
 
-for volumes in `aws ec2 describe-volumes  | grep available | awk '{print $9}' | grep vol| tr '\n' ' '`
+# ancienne commande ne fonctionne plus depuis le 06/08/2018 suite aux modifs côté AWS
+#for volumes in `aws ec2 describe-volumes  | grep available | awk '{print $9}' | grep vol| tr '\n' ' '`
+for volumes in `aws ec2 describe-volumes --filters Name=status,Values=available |grep VolumeId | awk '{print $4}'`
 do
         echo $volumes
  # supprimer les volumes available non-utilisés
